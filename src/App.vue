bead8b3a7af57bc3c309afb91883e2646dc9462e
moamenabdelrahman <moamenabdelrahman05@gmail.com>
2025-12-17 17:49:32 +0200

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="h-1500 bg-red-400">
+    <Navbar></Navbar>
+  </div>
 </template>
+
+<script>
+import Navbar from './Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style scoped></style>
---
+++ src/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed w-full">
+    <div class="flex px-10 h-25 items-center">
+      <div class="pressable text-2xl">LOGO</div>
+      <div class="flex gap-8 ms-15">
+        <div class="pressable">Home</div>
+        <div class="pressable">Services</div>
+        <div class="pressable">About us</div>
+        <div class="pressable">Contact us</div>
+        <div class="pressable">Location</div>
+      </div>
+      <button type="button" class="ms-auto pressable bg-white px-2.5 py-1.5">Book Now</button>
+    </div>
+    <div class="border w-85/100 mx-auto">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@reference "tailwindcss";
+
+.pressable { @apply
+  hover:cursor-pointer
+}
+</style>
